de33d51baba1e07d0ef99780226ba43db739ce1f
Ragini Ravichandren <rr986@scarletmail.rutgers.edu>
2024-10-17 02:20:16 -0400

    Add project files

--- src/db/dbConfig.js
+++
@@ -1,6 +0,0 @@
-//This file defines the basic configuration for the database (or local storage), so like It includes the database name, version, and tables.
-export const dbConfig = {
-   dbName: "HealthAssistantDB",
-   version: 1,
-   tables: ["healthData"],
- };
