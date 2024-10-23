d149d8187ec5df58dc6dd73a031ad8404ce3734a
OverkillLC <97358665+OverkillLC@users.noreply.github.com>
2024-10-23 15:44:55 +0000

    Implemented basic user account features, including sign-up, sign-in, and signing in using a google account.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { initializeApp } from "firebase/app";
 import {BrowserRouter} from 'react-router-dom';
+import fbconfig from './firebase/FirebaseConfig.js';
+
+const app = initializeApp(fbconfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
