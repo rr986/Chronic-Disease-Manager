1d78e62d5c13e4541089424d3cadcc59d15cc683
yuchitsai <ericatsai90825@gmail.com>
2024-10-26 22:34:12 -0400

    Update server host

--- server/models/reminderModel.js
+++ server/models/reminderModel.js
@@ -3,7 +3,7 @@ const { Pool } = pkg;
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: '192.168.1.100',
     database: 'reminders',
     password: 'elderecho',
     port: 5432
