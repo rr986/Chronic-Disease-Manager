83acd8a99b8cdf96aa4eae9cb2638cbeceab575c
yuchitsai <ericatsai90825@gmail.com>
2024-11-03 17:00:29 -0500

    Update package.json and Add vite.config.js for backend

---
+++ server/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        testTimeout: 10000,
+        environment: 'node', 
+        globals: true,       
+    },
+});
