c9a783746d9c0f375be58afc596b8b803692d11c
adamr6395 <aray7@stevens.edu>
2024-12-04 13:24:47 -0500

    Update Navigation.jsx

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -42,7 +42,7 @@ const NavigationAuth = () => {
             to='/voiceinput'
             className={(navData) => (navData.isActive ? 'active' : 'none')}
           >
-            VoiceInput
+            Voice Assistant
           </NavLink>
         </li>
         <li>
